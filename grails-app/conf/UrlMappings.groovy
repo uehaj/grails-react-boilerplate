e7d2f3143a8d049fb32a82877b64096e74da1cd3
UEHARA Junji <uehaj@jggug.org>
2015-12-24 06:30:02 +0900

    Remove unused controller mapping.

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,10 +1,6 @@
 class UrlMappings {
 
 	static mappings = {
-        "/api/books/$action?/$id?(.$format)?"{
-          controller = 'book'
-        }
-
         // Do not show index.gsp
         //        "/"(redirect:"/index.gsp")
         "500"(view:'/error')
