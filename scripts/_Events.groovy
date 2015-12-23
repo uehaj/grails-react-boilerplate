107a8b90a1df921be204b3b4226f371effe3892e
UEHARA Junji <uehaj@jggug.org>
2015-12-24 05:19:03 +0900

    Change when exception occured in comiling JS bundle not to terminating build process(consider running on heroku).

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -5,11 +5,17 @@ eventCompileStart = {kind ->
 }
 
 private void executeWebpack(){
-    // println "| Compile and bundle js files..."
-    // def proc = ["sh", "-c", "(cd react-app; npm run-script deploy)"].execute()
-    // proc.waitFor()
-    // if(proc.exitValue() != 0){
-    //     println "| Error! js compile failed : ${proc.err.text}"
-    // }
-    // println proc.text
+    try {
+		event("StatusUpdate", ["Compile and bundle js files..."])
+        def proc = ["sh", "-c", "(cd react-app; npm run-script deploy)"].execute()
+        proc.waitFor()
+        if(proc.exitValue() != 0){
+            println "| Error! js compile failed : ${proc.err.text}"
+        }
+        println proc.text
+		event("GenerateJsEnd", "Finished generation for Js bundle.")
+    }
+    catch (Exception e) {
+        e.printStackTrace()
+    }
 }
