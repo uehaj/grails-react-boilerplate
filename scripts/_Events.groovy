6f7cf307f45421e5ad08f29354a00da0d7537e40
UEHARA Junji <uehaj@jggug.org>
2015-12-24 04:45:17 +0900

    test for heroku.

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -5,11 +5,11 @@ eventCompileStart = {kind ->
 }
 
 private void executeWebpack(){
-    println "| Compile and bundle js files..."
-    def proc = ["sh", "-c", "(cd react-app; npm run-script deploy)"].execute()
-    proc.waitFor()
-    if(proc.exitValue() != 0){
-        println "| Error! js compile failed : ${proc.err.text}"
-    }
-    println proc.text
+    // println "| Compile and bundle js files..."
+    // def proc = ["sh", "-c", "(cd react-app; npm run-script deploy)"].execute()
+    // proc.waitFor()
+    // if(proc.exitValue() != 0){
+    //     println "| Error! js compile failed : ${proc.err.text}"
+    // }
+    // println proc.text
 }
